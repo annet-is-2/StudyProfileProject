5232c4a6d154ef8637c39a862dd53afc097f78ae
annet-is-2 <Anna_s.kovaleva@yahoo.com>
2021-10-11 23:42:31 +0300

    1st commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        /*University universityMGU = new University("123", "Moscow State University", "MGU",
+                                                  1835, University.StudyProfile.MEDICINE("Медицина"));
+
+         */
+
+        Student studentPeter = new Student("Peter Petrov", "123", 3, (float)99.87);
+
+        System.out.println(studentPeter);
+
+    }
+}
